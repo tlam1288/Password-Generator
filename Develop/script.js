5a6f0f2ab54780af080c5735bb42ad8c8db2046c
nhuanthi <tlam1288@gmail.com>
2020-06-10 20:24:37 -0700

    added if statements to see which types of characters are wanted by the user. variables were declared to store the user's choices

--- Develop/script.js
+++ Develop/script.js
@@ -9,16 +9,44 @@ function writePassword() {
   );
   //this makes sure number chosen in prompt is an integer
   var characterNum = Math.floor(number);
-  console.log(characterNum);
+  //console.log(characterNum);
   //verifies character count chosen is within range
   if (characterNum < 8 || characterNum > 128) {
     alert("Please choose a number between 8 - 128");
+    return false;
   }
 
-  var lowerCase = confirm("Do you want lowercase characters?");
-  var upperCase = confirm("Do you want uppercase characters?");
-  var numeric = confirm("Do you want numerics?");
-  var specialChar = confirm("Do you want special characters?");
+  //prompts to ask about character choices
+  var lowerChoice = confirm("Do you want lowercase characters?");
+  var upperChoice = confirm("Do you want uppercase characters?");
+  var numericChoice = confirm("Do you want numerics?");
+  var specialChoice = confirm("Do you want special characters?");
+
+  var lowerCase = false;
+  var upperCase = false;
+  var numeric = false;
+  var special = false;
+  var specialCharacters = ["!", "@", "#", "$", "%"];
+
+  if (lowerChoice === true) {
+    var lowerCase = Math.random().toString(36).substring(2, 15).toLowerCase();
+    console.log(lowerCase);
+  }
+
+  if (upperChoice === true) {
+    var upperCase = Math.random().toString(36).substring(2, 15).toUpperCase();
+    console.log(upperCase);
+  }
+
+  if (numericChoice === true) {
+    var numeric = Math.random();
+    console.log(numeric);
+  }
+
+  if (specialChoice === true) {
+    var special = specialCharacters[Math.floor(Math.random * 5)];
+    console.log(specialCharacters);
+  }
 
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
@@ -26,10 +54,11 @@ function writePassword() {
   //passwordText.value = password;
 
   //Testing to see if code to generate string works below
-  // var random =
-  //   Math.random().toString(36).substring(2, 15) +
-  //   Math.random().toString(36).substring(2, 15);
-  // alert(random);
+  var random =
+    // Math.random().toString(36).substring(2, 15) +
+    // Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15).toUpperCase();
+  alert(random);
 }
 
 // Add event listener to generate button
