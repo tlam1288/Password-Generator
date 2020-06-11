577200dd436de2af0537d0fdadb849ec567b6f4d
nhuanthi <tlam1288@gmail.com>
2020-06-10 21:33:55 -0700

    var total created to hold password strings. password now prints onto the text box after user selects criteria

--- Develop/script.js
+++ Develop/script.js
@@ -22,43 +22,66 @@ function writePassword() {
   var numericChoice = confirm("Do you want numerics?");
   var specialChoice = confirm("Do you want special characters?");
 
+  // these store the choices from user prompts about characters
   var lowerCase = false;
   var upperCase = false;
   var numeric = false;
   var special = false;
   var specialCharacters = ["!", "@", "#", "$", "%"];
+  var total = "";
 
+  //if true then lower case letters will be added
   if (lowerChoice === true) {
     var lowerCase = Math.random().toString(36).substring(2, 15).toLowerCase();
+    var total = total + lowerCase;
     console.log(lowerCase);
   }
 
+  //if true then upper case letters will be added
   if (upperChoice === true) {
     var upperCase = Math.random().toString(36).substring(2, 15).toUpperCase();
+    var total = total + upperCase;
     console.log(upperCase);
   }
 
+  //if true then numbers will be added
   if (numericChoice === true) {
-    var numeric = Math.random();
+    var numeric = Math.floor(Math.random() * 10);
+    var total = total + numeric;
     console.log(numeric);
   }
 
+  //if true then a special character will be added
   if (specialChoice === true) {
-    var special = specialCharacters[Math.floor(Math.random * 5)];
-    console.log(specialCharacters);
+    var special = specialCharacters[Math.floor(Math.random() * 5)];
+    var total = total + special;
+    console.log(special);
   }
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+  //var password = lowerCase + upperCase + numeric + special;
+  // getPassword();
+  // console.log(password);
 
-  //passwordText.value = password;
+  // function getPassword(characterNum) {
+  //   var total = lowerCase + upperCase + numeric + special;
+  //   var password = total.substring(2, characterNum);
+  //   console.log(password);
+  // }
+
+  //getPassword();
+
+  // var password = generatePassword();
+
+  //prints password into text box
+  var passwordText = document.querySelector("#password");
+  passwordText.value = total;
 
   //Testing to see if code to generate string works below
-  var random =
-    // Math.random().toString(36).substring(2, 15) +
-    // Math.random().toString(36).substring(2, 15) +
-    Math.random().toString(36).substring(2, 15).toUpperCase();
-  alert(random);
+  //var random =
+  // Math.random().toString(36).substring(2, 15) +
+  // Math.random().toString(36).substring(2, 15) +
+  // Math.random().toString(36).substring(2, 15).toUpperCase();
+  //alert(random);
 }
 
 // Add event listener to generate button
